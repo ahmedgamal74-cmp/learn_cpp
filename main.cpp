c23b99fe3ce3428acb96cd97a31fee38eebcb098
ahmedgamal74-cmp <eng.ahmed.gamal.cu@gmail.com>
2025-07-22 11:28:33 +0300

    git

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <bitset>
+#include <cctype>
+#include <climits>
+#include "typedefs.h"
+
+// using namespace std;
+
+int main() {
+	std::cout<<"Hello, Ahmed!"<<std::endl;
+	std::cout<<"-------------------------------------------------------------------------------------------"<<std::endl;
+
+	// unsigned short var1 = 0xF0F0;			// prunes the MSBs 
+	// cout << "Var1: 0x" << uppercase << hex << var1 << endl;	    
+	// cout << dec;
+
+	// cout << "Size of Double: " << sizeof(double) << endl;
+
+	// cout << "Int Max: " << INT_MAX << endl;
+
+	// cout << "Pi: " << M_PI << endl;
+
+	// cout << setw(3) << 6 << endl;
+	// cout << setw(3) << "+" << endl;
+	// cout << setw(3) << 4 << endl;
+	// cout << "---" << endl;
+	// cout << setw(3) << 10 << endl;
+	
+	// double ff = 0.12345;
+	// cout << ".2f: " << fixed << setprecision(2) << ff << endl; 
+
+	//string str1;
+	//cout << "Enter first name: ";
+	//cin >> str1;
+	//cout << "Str1: " << str1 << endl;	
+
+	//string line1;
+	//cout << "Enter full name: ";
+	//getline(cin, line1);
+	//getline(cin, line1);
+	//cout << "Line1: " << line1 << endl;
+
+	//string input1, input2, input3;
+	//cout << "Enter: First|Second|Third: ";
+	//getline(cin, input1, '|');
+	//getline(cin, input2, '|');
+	//cin >> input3;
+	//cout << "In1: " << input1 << "\tIn2: " << input2 << "\tIn3: " << input3 << endl;
+
+	// uint16 var3 = 1;
+	// cout<<"I am unit16: "<<var3<<endl;
+
+	uint32 var4 = 4;
+
+
+	std::cout<<"-------------------------------------------------------------------------------------------"<<std::endl;
+	std::cout<<"Goodbye, Ahmed!"<<std::endl;
+	return 0;
+}
---
+++ typedefs.h
@@ -0,0 +1,20 @@
+#ifndef TYPEDEFS_H
+#define TYPEDEFS_H
+
+#include <cstdint>
+#include <cstddef>
+
+typedef std::int8_t     int8;
+typedef std::uint8_t    uint8;
+typedef std::int16_t    int16;
+typedef std::uint16_t   uint16;
+typedef std::int32_t    int32;
+typedef std::uint32_t   uint32;
+typedef std::int64_t    int64;
+typedef std::uint64_t   uint64;
+
+typedef float   float32;
+typedef double  float64;
+typedef long double float128; 
+
+#endif // TYPEDEFS_H
